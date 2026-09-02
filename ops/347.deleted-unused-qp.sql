55905ab26d45eda44a43080d5726ba8ee9930d2a
erphise <github@erphise.com>
2026-09-02 12:38:03 +0000

    Deleted Unused QP

---
+++ ops/347.deleted-unused-qp.sql
@@ -0,0 +1,145 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Deleted Unused QP
+-- @exportedAt: 2026-09-02T12:37:59.936Z
+-- @opIds: 3729, 3730, 3731, 3732, 3733, 3734, 3735, 3736, 3737, 3738
+
+-- --- BEGIN op 3729 ( delete quality_profile "1080p Balanced" )
+delete from "quality_profile_tags" where "quality_profile_name" = '1080p Balanced';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '1080p Balanced';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '1080p Balanced';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '1080p Balanced';
+
+delete from "quality_groups" where "quality_profile_name" = '1080p Balanced';
+
+delete from "quality_profiles" where "name" = '1080p Balanced';
+-- --- END op 3729
+
+-- --- BEGIN op 3730 ( delete quality_profile "1080p Efficient" )
+delete from "quality_profile_tags" where "quality_profile_name" = '1080p Efficient';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '1080p Efficient';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '1080p Efficient';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '1080p Efficient';
+
+delete from "quality_groups" where "quality_profile_name" = '1080p Efficient';
+
+delete from "quality_profiles" where "name" = '1080p Efficient';
+-- --- END op 3730
+
+-- --- BEGIN op 3731 ( delete quality_profile "1080p Quality" )
+delete from "quality_profile_tags" where "quality_profile_name" = '1080p Quality';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '1080p Quality';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '1080p Quality';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '1080p Quality';
+
+delete from "quality_groups" where "quality_profile_name" = '1080p Quality';
+
+delete from "quality_profiles" where "name" = '1080p Quality';
+-- --- END op 3731
+
+-- --- BEGIN op 3732 ( delete quality_profile "1080p Quality HDR" )
+delete from "quality_profile_tags" where "quality_profile_name" = '1080p Quality HDR';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '1080p Quality HDR';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '1080p Quality HDR';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '1080p Quality HDR';
+
+delete from "quality_groups" where "quality_profile_name" = '1080p Quality HDR';
+
+delete from "quality_profiles" where "name" = '1080p Quality HDR';
+-- --- END op 3732
+
+-- --- BEGIN op 3733 ( delete quality_profile "1080p Remux" )
+delete from "quality_profile_tags" where "quality_profile_name" = '1080p Remux';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '1080p Remux';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '1080p Remux';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '1080p Remux';
+
+delete from "quality_groups" where "quality_profile_name" = '1080p Remux';
+
+delete from "quality_profiles" where "name" = '1080p Remux';
+-- --- END op 3733
+
+-- --- BEGIN op 3734 ( delete quality_profile "2160p Balanced" )
+delete from "quality_profile_tags" where "quality_profile_name" = '2160p Balanced';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '2160p Balanced';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '2160p Balanced';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '2160p Balanced';
+
+delete from "quality_groups" where "quality_profile_name" = '2160p Balanced';
+
+delete from "quality_profiles" where "name" = '2160p Balanced';
+-- --- END op 3734
+
+-- --- BEGIN op 3735 ( delete quality_profile "2160p Efficient" )
+delete from "quality_profile_tags" where "quality_profile_name" = '2160p Efficient';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '2160p Efficient';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '2160p Efficient';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '2160p Efficient';
+
+delete from "quality_groups" where "quality_profile_name" = '2160p Efficient';
+
+delete from "quality_profiles" where "name" = '2160p Efficient';
+-- --- END op 3735
+
+-- --- BEGIN op 3736 ( delete quality_profile "2160p Quality" )
+delete from "quality_profile_tags" where "quality_profile_name" = '2160p Quality';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '2160p Quality';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '2160p Quality';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '2160p Quality';
+
+delete from "quality_groups" where "quality_profile_name" = '2160p Quality';
+
+delete from "quality_profiles" where "name" = '2160p Quality';
+-- --- END op 3736
+
+-- --- BEGIN op 3737 ( delete quality_profile "2160p Remux" )
+delete from "quality_profile_tags" where "quality_profile_name" = '2160p Remux';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '2160p Remux';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '2160p Remux';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '2160p Remux';
+
+delete from "quality_groups" where "quality_profile_name" = '2160p Remux';
+
+delete from "quality_profiles" where "name" = '2160p Remux';
+-- --- END op 3737
+
+-- --- BEGIN op 3738 ( delete quality_profile "720p Quality" )
+delete from "quality_profile_tags" where "quality_profile_name" = '720p Quality';
+
+delete from "quality_profile_languages" where "quality_profile_name" = '720p Quality';
+
+delete from "quality_profile_qualities" where "quality_profile_name" = '720p Quality';
+
+delete from "quality_profile_custom_formats" where "quality_profile_name" = '720p Quality';
+
+delete from "quality_groups" where "quality_profile_name" = '720p Quality';
+
+delete from "quality_profiles" where "name" = '720p Quality';
+-- --- END op 3738
