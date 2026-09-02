2bf36f2ff126a37d1152bc191d57ed79290446d7
erphise <github@erphise.com>
2026-09-02 12:38:28 +0000

    Updated Erphise's QP

---
+++ ops/348.updated-erphise-s-qp.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Updated Erphise's QP
+-- @exportedAt: 2026-09-02T12:38:25.476Z
+-- @opIds: 3739
+
+-- --- BEGIN op 3739 ( update quality_profile "1080p Compact Erphise" )
+update "quality_profiles" set "name" = '1080p Compact Erphise' where "name" = '1080p Compact';
+-- --- END op 3739
